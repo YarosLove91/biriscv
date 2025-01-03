7c9ed8b59eb1530607e7e060a5902b800152454a
meshcheryakov <Meshcheryakov_ye@rnd.stcnet.ru>
2025-01-03 20:31:16 +0300

    [WIP] Debugging information will appear in the main file, tracing will be updated.

--- tb/tb_top/main.cpp
+++ tb/tb_top/main.cpp
@@ -62,8 +62,10 @@ static void exit_override(void)
 //--------------------------------------------------------------------
 void vl_finish (const char* filename, int linenum, const char* hier)
 { 
+    std::cout << "\033[32m\nExit success!\n\033[0m" << std::endl;
+    std::cout << "Filename is\t" << filename << "linenum is:\t" << linenum << "\thier is \t" << hier << endl;
     // Jump to exit handler!
-    exit(0);    
+    exit(EXIT_SUCCESS);    
 }
 //-----------------------------------------------------------------
 // sigint_handler
@@ -71,9 +73,9 @@ void vl_finish (const char* filename, int linenum, const char* hier)
 static void sigint_handler(int s)
 {
     exit_override();
-
+    std::cerr << "\033[31m\nExit failure!\n\033[0m Code erros is:\t" << s << std::endl;
     // Jump to exit handler!
-    exit(1);
+    exit(EXIT_FAILURE);
 }
 //--------------------------------------------------------------------
 // sc_main
@@ -144,14 +146,15 @@ int sc_main(int argc, char* argv[])
     tb->CLK0_NAME(CLK0_NAME);
     tb->RST0_NAME(clk0_rst.rst);
 
+    sc_core::sc_start(SC_ZERO_TIME);
     // Waves
     if (trace)
         tb->add_trace(sc_create_vcd_trace_file(vcd_name), "");
 
     tb->set_argcv(argc - last_argc, &argv[last_argc]);
-
+    tb->enable_verilator_trace();
     // Go!
-    sc_start();
+    sc_core::sc_start();
 
     return 0;
 }
